daa1ab8f532f1c5ba0fe3d0722aaa6f5cdba8ff6
Gabriel Wojcik <gabrielwojcikv@gmail.com>
2023-07-04 20:32:00 -0300

    FEAT - Added new fonts and layout for deskotop

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import './styles.css';
 
-
-
 export default function Button(props) {
-    console.log('btn-props',props)
     return(
             <button>{props.children}</button>
     )
